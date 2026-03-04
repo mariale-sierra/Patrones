b047537ab7d27193c5a14960bdc16a2af334edfb
EmilyGG24308 <gon24308@uvg.edu.gt>
2026-03-04 16:22:37 -0600

    Update Main.java
    parte del main sin menu.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,55 @@
+    //infrastructure
+DatabaseContext databaseContext = new DatabaseContext();
 IPedidoRepository repository = new PedidoRepository();
 IUnitOfWork unitOfWork = new UnitOfWork();
 
 IPedidoService service = new PedidoService(repository, unitOfWork);
 
 // Decorators (tu parte)
-service = new ValidationDecorator(service);
-service = new LoggingDecorator(service);
-service = new SecurityDecorator(service);
+pedidoService = new ValidationDecorator(pedidoService);
+pedidoService = new LoggingDecorator(pedidoService);
+pedidoService = new SecurityDecorator(pedidoService);
+
+package main;
+
+import application.IPedidoService;
+import application.PedidoService;
+import application.decorators.LoggingDecorator;
+import application.decorators.SecurityDecorator;
+import application.decorators.ValidationDecorator;
+import domain.PedidoPrototype;
+import infrastructure.DatabaseContext;
+import infrastructure.IPedidoRepository;
+import infrastructure.IUnitOfWork;
+import infrastructure.PedidoRepository;
+import infrastructure.UnitOfWork;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+        PedidoPrototype prototype = new PedidoPrototype();
+
+        IPedidoService pedidoService = new PedidoService(
+                repository,
+                unitOfWork,
+                prototype
+        );
+
+        
+
+
+        // simulacion
+        System.out.println("-------- CREAR PEDIDO -------- ");
+        pedidoService.crearPedido(1);
+
+        System.out.println("-------- CONFIRMAR PEDIDO -------- ");
+        pedidoService.confirmarPedido(1);
+
+        System.out.println("-------- CANCELAR PEDIDO -------- ");
+        pedidoService.cancelarPedido(1);
+    }
+}
